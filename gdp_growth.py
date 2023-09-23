7cabe0ac2bee50335b5629a66ab3484f11d6aad8
Mamduh Halawa <mamduhkod@gmail.com>
2023-09-23 15:02:05 +0200

    shifted to 30 last years

--- gdp_growth.py
+++ gdp_growth.py
@@ -2,14 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('gdp_growth.csv')
-
-data = data[1:10]
-data.drop(columns='Code')
-data.fillna(0)
+data = data.set_index('Country Name')
+data = data.T
+data = data.iloc[30:]
 data
 
-plt.figure(figsize=(15,5))
-plt.plot(data['Country Name'], data['2012'],color='b',label='GDP')
-plt.xlabel("Country")
+# map the growth of countries GDP for last 20 years
+
+plt.figure = figsize=((100,50))
+plt.plot(data.index, data.columns,color='b',label='GDP')
+plt.xlabel("Year")
 plt.ylabel("GDP")
 plt.title('GDP per country')
