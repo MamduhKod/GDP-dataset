188962a6f87632bbef6b9ac5fa3c1236813fe2ba
Mamduh Halawa <mamduhkod@gmail.com>
2023-09-25 11:55:38 +0200

    Trying to visualize top growth overlap with prev

--- gdp_growth.py
+++ gdp_growth.py
@@ -11,8 +11,10 @@ data1
 data1 = data1.set_index('Country Name')
 data1 = data1.T
 data1 = data1.iloc[31:]
-data1.iloc[0]
+data1.loc['1990']
 data1
+
+
 
 #Data on mental health
 data2 = pd.read_csv('/Users/mamduhhalawa/Desktop/GDP dataset/share-with-depression.csv')
@@ -20,29 +22,46 @@ data2 = pd.read_csv('/Users/mamduhhalawa/Desktop/GDP dataset/share-with-depressi
 data2 = data2.set_index("Year")
 data2
 data2.rename(columns={'Prevalence - Depressive disorders - Sex: Both - Age: Age-standardized (Percent)':'Prevalence'}, inplace=True)
+data2
 
 
-#take 1990 as example
+#Visualize relationship between country growth rates in 1990, and their prevalence of mental health issues
 
 prev_1990 = data2[data2["Year"] == 1990]
-prev_1990.rename(columns={'Prevalence - Depressive disorders - Sex: Both - Age: Age-standardized (Percent)':'Prevalence'}, inplace=True)
 prev_1990.set_index('Entity')
 
-prev_1990.merge(data1)
+prev_1990_sorted=prev_1990.sort_values(by='Prevalence',ascending=False)
+
+prev_1990_sorted.plot(y='Prevalence', kind='bar')
+prev_1990
+
+growth_1990 = data1.loc['1990']
+growth_frame = pd.DataFrame(growth_1990)
+sorted_growth = growth_frame['1990'].sort_values(ascending=False)
+
+# Investigate the 30 highest growing economies and their prevalence of mental health issues
+top_growth = sorted_growth.iloc[:30]
+top_growth = pd.DataFrame(top_growth)
+top_growth
+
+top_prev = prev_1990_sorted.iloc[:30]
+top_prev.merge(top_growth)
+top_growth.rename(columns={'1990':'Growth rate 1990'}, inplace=True)
+top_growth
+
+result = pd.concat([top_growth, top_prev], axis=1)
+result.info()
+
+result = result['Year'].convert_dtypes(str)
+result.info()
+result 
 
 
-summed_prev = []
 
-for entity in data2['Entity']:
-        if entity in summed_prev:
-            continue
-        summed_prev.append(entity)
-        
 
-summed_prev
 
-for country in summed_prev:
-    data2['Prevalence'].sum()
+
+
 
     
     
